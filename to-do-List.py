940da98554fad0f0214f3e3c21ba205380beaabb
Ab1ga1l-tech <150353834+Ab1ga1l-tech@users.noreply.github.com>
2024-12-10 16:18:11 -0300

    Update to-do-List.py

--- to-do-List.py
+++ to-do-List.py
@@ -1,11 +1,13 @@
 lista = []
-print(" Bem-vindo a sua to-do list \n Digite:"
-      "[1] adicionar tarefa"
-      "[2] ver tarefas"
-      "[3] excluir "
-      "[4] concluido"
-      "[5] sair")
+print(" Bem-vindo a sua to-do list\n 
+     Digite os seguintes valores:\n
+     [1] adicionar tarefa \n
+     [2] ver tarefas\n
+     [3] excluir "\n
+     [4] concluido\n
+     [5] sair")
 valor = input("digite um valor")
+
 tarefa = input("uma tarefa")
 lista.append(tarefa)
 print(tarefa)
